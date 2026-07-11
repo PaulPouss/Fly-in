300ac88fc15c9c73d6e78700984e701b6f3b4a03
Paul Andiazabal <paul.andiazabal@gmail.com>
2026-07-11 11:31:28 +0200

    pudate

--- parser.py
+++ parser.py
@@ -29,39 +29,36 @@ class Hub ():
 class NormalHub(Hub):
     def __init__(self, name: str, coord_x: int, coord_y: int, color: str,
                  max_drone: int = 1) -> None:
-        super().__init__(self, name, coord_x, coord_y, color,
-                 max_drone)
+        super().__init__(name, coord_x, coord_y, color, max_drone)
 
 
 class BlockedHub(Hub):
     def __init__(self, name: str, coord_x: int, coord_y: int, color: str,
                  max_drone: int = 1):
-        super().__init__(self, name, coord_x, coord_y, color,
-                 max_drone)
+        super().__init__(name, coord_x, coord_y, color, max_drone)
         self.blocked: str = "BLOCKED"
 
 
 class RestrictedHub(Hub):
     def __init__(self, name: str, coord_x: int, coord_y: int, color: str,
                  max_drone: int = 1):
-        super().__init__(self, name, coord_x, coord_y, color,
-                 max_drone)
+        super().__init__(name, coord_x, coord_y, color, max_drone)
 
 
 class PriotityHub(Hub):
     def __init__(self, name: str, coord_x: int, coord_y: int, color: str,
                  max_drone: int = 1):
-        super().__init__(self, name, coord_x, coord_y, color,
-                 max_drone)
+        super().__init__(name, coord_x, coord_y, color, max_drone)
 
 
 class connection():
-    def __init__(self, name: str, first_hub: Hub, second_hub: Hub,max_link_capacity: int = 1):
+    def __init__(self, name: str, first_hub: Hub, second_hub: Hub,
+                 max_link_capacity: int = 1):
         self.name = name
         self.first_hub = first_hub
         self.second_hub = second_hub
         self.is_free = True
-        self.max_link_capacity =max_link_capacity
+        self.max_link_capacity = max_link_capacity
         pass
 
 
@@ -71,11 +68,6 @@ class map_info():
         self.drone_numbers = drone_number
         self.hub_list: list[Hub] = list_hub
         self.connection_list: list[connection] = list_connection
-
-
-class DataProcessor():
-    def __init__(self):
-        self.data: list[Any] = []
 
 
 def parser_monitor():
--- test_drone.py
+++ test_drone.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from typing import Any
 import sys
 
+
 class DataProcessor(ABC):
     def __init__(self):
         self.data: list[Any] = []
@@ -21,6 +22,21 @@ class DataProcessor(ABC):
     @abstractmethod
     def ingest(self, data: Any) -> None:
         pass
+
+
+class StreamProcessor():
+    def __init__(self):
+        self.processors: list[DataProcessor] = []
+
+    def add_processor(self, processor: DataProcessor) -> None:
+        for current_processors in self.processors:
+            if type(current_processors) is type(processor):
+                return
+        self.processors.append(processor)
+
+    def ingest_stream(self, data: Any) -> None:
+        for processors in self.processors:
+            processors.ingest(data)
 
 
 class HubProcessor(DataProcessor):
@@ -48,14 +64,41 @@ class HubProcessor(DataProcessor):
         self.data.append(data)
 
 
+class ConnectionProcessor(DataProcessor):
+    def validate(self, data: str) -> bool:
+        try:
+            kind, value = data.split(":", 1)
+            if kind != "connection":
+                return False
+            parts = value.split()
+
+            if len(parts) < 1:
+                return False
+
+        except ValueError:
+            return False
+        return True
+
+    def ingest(self, data: Any) -> None:
+        if not self.validate(data):
+            return
+
+        self.data.append(data)
+
+
 def main() -> None:
     filename = sys.argv[1]
+    parser = StreamProcessor()
     processor = HubProcessor()
+    connection = ConnectionProcessor()
+    parser.add_processor(processor)
+    parser.add_processor(connection)
     with open(filename) as f:
         data = f.read().splitlines()
         for lines in data:
-            processor.ingest(lines)
-    print(processor.data)
+            parser.ingest_stream(lines)
+    for processors in parser.processors:
+        print(processors.data)
 
 
 if __name__ == "__main__":
