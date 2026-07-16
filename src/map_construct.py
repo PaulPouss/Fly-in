a0e0afb3156bb1d5d28466bdc22285235cbeb034
Paul Andiazabal <paul.andiazabal@gmail.com>
2026-07-16 15:23:52 +0200

    update

--- src/map_construct.py
+++ src/map_construct.py
@@ -4,5 +4,13 @@ class MapConst():
     def add_hub(self, line_number: int, data: str) -> None:
         kind, value = data.split(":", 1)
         parts = value.split()
-        if len(parts) < 4:
-            
+        name: str = parts[0]
+        x: int = int(parts[1])
+        y: int = int(parts[2])
+        metadata = {}
+        if len(parts) > 3:
+            parts[3].strip("[]")
+            for items in parts[3].split():
+                key, value = items.split("=", 1)
+                metadata[key] = value
+
